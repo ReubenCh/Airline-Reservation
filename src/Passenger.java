9bf1082fb930e84e82d4cabf1927df8df14c1c21
Sagar Patel <s.72427patel@gmail.com>
2015-12-21 14:52:48 -0500

    Methods

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,4 +1,76 @@
 
+
 public class Passenger {
+	
+	private String firstName, lastName, seatNumber, aisle, name; 
+	public int SORT_BY_NAME;
+	public int SORT_BY_SEAT;
+	public int sortValue; 
+	public int rowNumber; 
+	 
+	public Passenger(){
+		
+	}
+	
+	public Passenger(String first, String last){
+		
+	}
+	
+	public Passenger(String first, String last, int row, String aisle){
+		
+	}
+	
+//	+ compareTo(p: Passenger): int
+//	+ toString(): String
+	
+	public String getAisle(){
+		return aisle; 
+	}
+	
+	public String getFirstName(){
+		return firstName; 
+	}
+
+	public String getLastName(){
+		return lastName; 
+	}
+	
+	public String getName(){
+		return name; 
+	}
+	
+	public int getRowNumber(){
+		return rowNumber; 
+	}
+	
+	public String getSeatNumber(){
+		return seatNumber; 
+	}
+	
+	public void setAisle(String letter){
+		aisle = letter; 
+	}
+	
+	public void setFirstName(String first){
+		firstName = first; 
+	}
+	
+	public void setLastName(String last){
+		lastName = last; 
+	}
+	
+	public void setName(String first, String last){
+		name = first + ", " + last; 
+	}
+	
+	public void setRowNumber(int num){
+		rowNumber = num; 
+	}
+	
+	public void setSeatNumber(String seatNumber){
+		//Check
+		this.seatNumber = seatNumber; 
+	}
+	
 	
 }
